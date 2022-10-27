bd7986e1047882a77d7c4e824d9c2a0d3b6ce60a
anggiwahyuu <anggiwahyu543@gmail.com>
2022-10-28 05:38:03 +0700

    modify vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/agera-digital-website/'
     : '/'
-}
+})
